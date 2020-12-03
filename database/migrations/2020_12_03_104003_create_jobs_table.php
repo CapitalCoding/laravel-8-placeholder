094449be4748cfe3fa72cf01a9820f387b5451bf
capitalcoding <diogomrn@gmail.com>
2020-12-03 08:41:12 -0300

    Inicial

---
+++ database/migrations/2020_12_03_104003_create_jobs_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateJobsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('jobs', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->index(); //creator
+            $table->string('job_name');
+            $table->string('description');
+            $table->unsignedDecimal('payout');
+            $table->string('currency');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('jobs');
+    }
+}
---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,24 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                <x-table>
+                    <x-slot name="head">
+                        <x-table.heading>
+                            Hey
+                        </x-table.heading>
+                    </x-slot>
+                    <x-slot name="body">
+
+                    </x-slot>
+                </x-table>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
